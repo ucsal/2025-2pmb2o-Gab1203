8e318b2d42c5ebd67ce73affaa61d9cf299373da
GabrielFraga <gabrielbff2006@gmail.com>
2025-08-27 21:34:41 -0300

    feat: implementação de HashMap para injeção de dependências em DiscountCalculator.java

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,27 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DiscountCalculator{
 
+    private Map<CustomerType, DiscountPolicy> policyMap;
+
+    public DiscountCalculator(){
+
+        policyMap =  new HashMap<>();
+        policyMap.put(CustomerType.REGULAR, new RegularPolicy());
+        policyMap.put(CustomerType.PREMIUM, new PremiumPolicy());
+        policyMap.put(CustomerType.PARTNER, new PartnerPolicy());
+
+    }
+
+    public DiscountCalculator(Map<CustomerType, DiscountPolicy> policyMap){
+        this.policyMap = policyMap;
+    }
 
     public double apply(double amount, CustomerType type){
-
-
-        return 10.0;
+        DiscountPolicy policy = policyMap.get(type);
+        return policy.apply(amount);
     }
 }
